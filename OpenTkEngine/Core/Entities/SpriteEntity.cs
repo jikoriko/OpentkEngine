dc5ce5ef8edb75f82a763832d770e144a3ead4c6
jikoriko <perry-swain20@hotmail.co.uk>
2015-07-08 03:21:20 +0100

    basic sprite entity

---
+++ OpenTkEngine/Core/Entities/SpriteEntity.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTkEngine.Core
+{
+    public class SpriteEntity : Entity
+    {
+        protected Texture _texture;
+
+        public SpriteEntity(float x, float y, float z, string spriteName)
+            : base(x, y, z)
+        {
+            _texture = Assets.GetTexture(spriteName);
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            Graphics.DrawTexture(_texture, _position.X, _position.Y, _position.Z, Color4.White);
+        }
+    }
+}
